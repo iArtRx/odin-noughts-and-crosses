7e820061ef8d5bfbb79c29a68cdd4be6f9f6840f
Art <iArtRx@protonmail.com>
2023-01-06 15:09:40 +0000

    highlights the winning squares and locks the gameboard

--- script.js
+++ script.js
@@ -41,66 +41,78 @@ const NoughtsAndCrosses = (() => {
           square.classList.add("mark-value");
           gameboardDisplay.appendChild(square);
       }
-    }  
+    }
+    
+  let gameover = false;
 
   const addMark = () => {  
     const squares = document.querySelectorAll(".board-square"); 
     squares.forEach((square) => {
       square.addEventListener("click", () => {
-        const squareIndex = square.getAttribute("data-index");
-        // Check to see if square is occupied, if not then place the active player's mark with animation.
-        if (gameboard[squareIndex] == "") {
-          const mark = PlayerTurn.getTurn();
-          gameboard[squareIndex] = mark;;
-          square.animate(
-            [
-              { transform: "scale(1)" },
-              { transform: "scale(1.2)" },
-              { transform: "scale(1)" },
-            ],
-            {
-              duration: 300,
-              easing: "ease-out",
+        if (!gameover) {
+          const squareIndex = square.getAttribute("data-index");
+          // Check to see if square is occupied, if not then place the active player's mark with animation.
+          if (gameboard[squareIndex] == "") {
+            const mark = PlayerTurn.getTurn();
+            gameboard[squareIndex] = mark;;
+            square.animate(
+              [
+                { transform: "scale(1)" },
+                { transform: "scale(1.2)" },
+                { transform: "scale(1)" },
+              ],
+              {
+                duration: 300,
+                easing: "ease-out",
+              }
+            );
+            // Image used to mark player and its folder location.  
+            const markURL = (mark === "X") ? "./images/delete-cross.png": "./images/moon-hand-drawn-circle.png";
+            const markIcon = new Image();
+            markIcon.src = markURL;
+            square.appendChild(markIcon);
+            square.setAttribute("mark-value", mark);
+            // Toggle player turn and updates the scoreboard to indicate this.
+            PlayerTurn.toggleTurn();
+            renderScoreboard(playerOne, playerTwo);
+            // Check result and displays appropitate messagel;
+            const winner = checkWinner();
+            const resultDisplay = document.querySelector(".result-display");
+            const winnerMark = gameboard[winner[0]];
+  
+            if (winner === "Draw") {
+              resultDisplay.innerHTML = "It's a Draw!";
+              gameover = true;
             }
-          );
-          // Image used to mark player and its folder location.  
-          const markURL = (mark === "X") ? "./images/delete-cross.png": "./images/moon-hand-drawn-circle.png";
-          const markIcon = new Image();
-          markIcon.src = markURL;
-          square.appendChild(markIcon);
-          square.setAttribute("mark-value", mark);
-          // Toggle player turn and updates the scoreboard to indicate this.
-          PlayerTurn.toggleTurn();
-          renderScoreboard(playerOne, playerTwo);
-          // Check result and displays appropitate messagel;
-          const winner = checkWinner();
-          const resultDisplay = document.querySelector(".result-display");
-          const winnerMark = gameboard[winner[0]];
-
-          if (winner === "Draw") {
-            resultDisplay.innerHTML = "It's a Draw!";
-          }
-          else
-          {
-              const winnerString = "The Winner was";
-              resultDisplay.innerHTML = (winnerMark === "X") ? `${winnerString} ${playerOne.value}!` : `${winnerString} ${playerTwo.value}!`;
-              
-          }
-          const replay = document.querySelector("#replay");
-          replay.style.display = "block";
-          replay.addEventListener("click", restart);
-        }      
+            else
+            {   
+                const winnerString = "The Winner was";
+                resultDisplay.innerHTML = (winnerMark === "X") ? `${winnerString} ${playerOne.value}!` : `${winnerString} ${playerTwo.value}!`;
+  
+                for (let index of winner) {
+                  const winSquare = document.querySelector(`[data-index="${index}"]`);
+                  winSquare.classList.add((winnerMark === "X" ? "winner-navy" : "winner-pink"));
+                }
+                gameover = true;
+            }
+            const replay = document.querySelector("#replay");
+            replay.style.display = "block";
+            replay.addEventListener("click", restart);
+          }      
+        }
       });
     });
   }
-
+  
   const restart = () => {
     const squares = document.querySelectorAll('.board-square');
     squares.forEach((square) => {
       square.innerHTML = '';
       square.setAttribute('mark-value', '');
+      square.classList.remove("winner-navy", "winner-pink")
     });
     gameboard = ['', '', '', '', '', '', '', '', ''];
+    gameover = false;
   
     const resultDisplay = document.querySelector('.result-display');
     resultDisplay.innerHTML = '';
