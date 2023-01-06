6e4aeabde4264f304fed02589663238472932e25
Art <iArtRx@protonmail.com>
2023-01-06 08:31:57 +0000

    new results display, added and fixed css.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const NoughtsAndCrosses = (() => {
   const gameArena = document.querySelector(".game-arena");
   const startMenu = document.querySelector(".start-menu");
-
+  const playerOne = document.getElementById("player-one");
+  const playerTwo = document.getElementById("player-two");
+  
   const startGame = () => {
     gameArena.style.display = "grid";
     startMenu.style.display = "none";
-    const playerOne = document.getElementById("player-one");
-    const playerTwo = document.getElementById("player-two");
+    renderScoreboard(playerOne, playerTwo);
   }
 
   const start = document.querySelector("#start");
   start.addEventListener("click", startGame)
 
-  const scoreboard = document.querySelectorAll(".scoreboard");
-  const renderScoreboard = () => {
-    
+  const renderScoreboard = (playerOne, playerTwo) => {
+    const playerOneName = document.getElementById("player-one-name");
+    const playerTwoName = document.getElementById("player-two-name");
+
+    playerOneName.textContent = playerOne.value;
+    playerTwoName.textContent = playerTwo.value;
+
+    const activePlayer = PlayerTurn.getTurn();
+
+    let playerOneIcon = document.querySelector("#player-one-score .user-icon");
+    let playerTwoIcon = document.querySelector("#player-two-score .user-icon");
+
+    playerOneIcon.src = (activePlayer === "X") ? "./images/user-navy-full.png" : "./images/user-navy.png";
+    playerTwoIcon.src = (activePlayer === "X") ? "./images/user-pink.png" : "./images/user-pink-full.png";
   }
+
 
   const gameboardDisplay = document.querySelector(".gameboard");
   let gameboard = ["", "", "", "", "", "", "", "", ""];
@@ -35,6 +48,7 @@ const NoughtsAndCrosses = (() => {
     squares.forEach((square) => {
       square.addEventListener("click", () => {
         const squareIndex = square.getAttribute("data-index");
+        // Check to see if square is occupied, if not then place the active player's mark with animation.
         if (gameboard[squareIndex] == "") {
           const mark = PlayerTurn.getTurn();
           gameboard[squareIndex] = mark;;
@@ -49,25 +63,36 @@ const NoughtsAndCrosses = (() => {
               easing: "ease-out",
             }
           );
-
-          const imgURL = (mark === "X") ? "./images/delete-cross.png": "./images/moon-hand-drawn-circle.png";
+          // Image used to mark player and its folder location.  
+          const markURL = (mark === "X") ? "./images/delete-cross.png": "./images/moon-hand-drawn-circle.png";
           const markIcon = new Image();
-          markIcon.src = imgURL;
+          markIcon.src = markURL;
           square.appendChild(markIcon);
           square.setAttribute("mark-value", mark);
+          // Toggle player turn and updates the scoreboard to indicate this.
           PlayerTurn.toggleTurn();
+          renderScoreboard(playerOne, playerTwo);
+          // Check result and displays appropitate messagel;
           const winner = checkWinner();
-          if (winner !== null) {
-            alert(winner);
-          }      
-        }
+          const resultDisplay = document.querySelector(".result-display");
+          const winnerMark = gameboard[winner[0]];
+
+          if (winner === "Draw") {
+            resultDisplay.innerHTML = "It's a Draw!";
+          }
+          else
+          {
+              const winnerString = "The Winner was";
+              resultDisplay.innerHTML = (winnerMark === "X") ? `${winnerString} ${playerOne.value}!` : `${winnerString} ${playerTwo.value}!`;
+          }
+        }      
       });
     });
-
   }
 
   const checkWinner = () => {
-    const combinations = [    [0, 1, 2],
+    const combinations = [    
+      [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
       [0, 3, 6],
@@ -80,10 +105,10 @@ const NoughtsAndCrosses = (() => {
     for (let i = 0; i < combinations.length; i++) {
       const [a, b, c] = combinations[i];
       if (gameboard[a] !== "" && gameboard[a] === gameboard[b] && gameboard[b] === gameboard[c]) {
-        return `Player ${gameboard[a]} wins!`;
+        return combinations[i];
       }
     }
-    return null;
+    return gameboard.every((square) => square !== "") ? "Draw" : null;
   };
   
 
@@ -102,21 +127,16 @@ const PlayerTurn = (() => {
   }
 })();
 
-
-   
-
   return {
     startGame: startGame,
     renderGameboard: renderGameboard,
     addMark: addMark,
+    renderScoreboard : renderScoreboard,
   }
 })();
 
-
-
-// To render the gameboard
+// Render Gameboard and initialise addMark function
 NoughtsAndCrosses.renderGameboard();
 NoughtsAndCrosses.addMark();
-
 
   
